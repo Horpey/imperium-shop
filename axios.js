33db95a41642dc169201e5b7bf8a5cd00bbf812a
horpey <adeniran1059@gmail.com>
2021-08-23 02:33:54 +0100

    Clear old files

--- axios.js
+++
@@ -1,8 +0,0 @@
-// axios
-import axios from "axios";
-const baseURL = "https://altpower-staging.herokuapp.com/v1/";
-
-export default axios.create({
-  baseURL
-  // You can add your headers here
-});
--- util/auth.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-export function setAuthToken(token) {
-  axios.defaults.headers.common["Authorization"] = `bearer ${token}`;
-}
-export function resetAuthToken() {
-  delete axios.defaults.headers.common["Authorization"];
-}
